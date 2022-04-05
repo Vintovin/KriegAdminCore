b26342a43400ff0d1b422bf02f5ba5511021d4f6
Bimbles <64385517+Vintovin@users.noreply.github.com>
2022-04-06 00:32:07 +0100

    Update demo.lua

--- Commands/demo.lua
+++ Commands/demo.lua
@@ -4,10 +4,15 @@ local Com = {}
 Com.Pref = {
 	["AdminLevel"] = 2,
 	["CommandHelp"] = "Demo Command | :demo"
+	
 }
 
-Com.Main = function()
-	print("Demo")
+Com.Main = function(Params)
+	print("--Demo--")
+	for i,v in pairs(Params) do
+		print("Parameter "..i..":"..v)
+	end
+	
 	return(true)
 end
 
